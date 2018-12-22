18a24f65b1d64c15ebb6f139c6c13b221087d079
jizhi <theniceangel@163.com>
2018-12-22 09:25:02 +0800

    complete promise

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 const JPromise = require('./lib')
 
-// const pResolve = new JPromise((resolve) => {
-//   resolve(1)
-// })
+const pResolve = new JPromise((resolve) => {
+  resolve(1)
+})
 
-// console.log(pResolve)
+console.log(pResolve)
 
-// const pReject = new JPromise((resolve, reject) => {
-//   reject(2)
-// })
+const pReject = new JPromise((resolve, reject) => {
+  reject(2)
+})
 
-// console.log(pReject)
+console.log(pReject)
 
-// const pThen = new JPromise((resolve) => {
-//   resolve(3)
-// }).then((x) => {
-//   console.log(x)
-// })
+const pThen = new JPromise((resolve) => {
+  resolve(3)
+}).then((x) => {
+  console.log(x)
+})
 
-// const pDelayedResolve = new JPromise((resolve) => {
-//   setTimeout(() => {
-//     resolve(3)
-//   }, 3000)
-// }).then((x) => {
-//   console.log(x)
-// })
+const pDelayedResolve = new JPromise((resolve) => {
+  setTimeout(() => {
+    resolve(3)
+  }, 3000)
+}).then((x) => {
+  console.log(x)
+})
 
 const pResovePromise = new JPromise((resolve) => {
   resolve(new JPromise((resolve) => {
@@ -34,5 +34,33 @@ const pResovePromise = new JPromise((resolve) => {
 setTimeout(() => {
   console.log(pResovePromise)
 }, 1000)
+
+const pAllPromise = [new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 1000)
+}), new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(2)
+  }, 5000)
+})]
+
+JPromise.all(pAllPromise).then((res) => {
+  console.log(res)
+})
+
+const pRacePromise = [new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1)
+  }, 10000)
+}), new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(2)
+  }, 5000)
+})]
+
+JPromise.race(pRacePromise).then((res) => {
+  console.log(res)
+})
 
 console.log('test')
