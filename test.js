b83b0ace6eeed2c7b931d9fb136e0a3483ea8ab3
jizhi <theniceangel@163.com>
2018-12-22 23:35:42 +0800

    bugfix

--- test.js
+++ test.js
@@ -1,53 +1,53 @@
 const JPromise = require('./lib')
 
-const pResolve = new JPromise((resolve) => {
-  resolve(1)
-})
+// const pResolve = new JPromise((resolve) => {
+//   resolve(1)
+// })
 
-console.log(pResolve)
+// console.log(pResolve)
 
-const pReject = new JPromise((resolve, reject) => {
-  reject(2)
-})
+// const pReject = new JPromise((resolve, reject) => {
+//   reject(2)
+// })
 
-console.log(pReject)
+// console.log(pReject)
 
-const pThen = new JPromise((resolve) => {
-  resolve(3)
-}).then((x) => {
-  console.log(x)
-})
+// const pThen = new JPromise((resolve) => {
+//   resolve(3)
+// }).then((x) => {
+//   console.log(x)
+// })
 
-const pDelayedResolve = new JPromise((resolve) => {
-  setTimeout(() => {
-    resolve(3)
-  }, 3000)
-}).then((x) => {
-  console.log(x)
-})
+// const pDelayedResolve = new JPromise((resolve) => {
+//   setTimeout(() => {
+//     resolve(3)
+//   }, 3000)
+// }).then((x) => {
+//   console.log(x)
+// })
 
-const pResovePromise = new JPromise((resolve) => {
-  resolve(new JPromise((resolve) => {
-    resolve(2)
-  }))
-})
-setTimeout(() => {
-  console.log(pResovePromise)
-}, 1000)
+// const pResovePromise = new JPromise((resolve) => {
+//   resolve(new JPromise((resolve) => {
+//     resolve(2)
+//   }))
+// })
+// setTimeout(() => {
+//   console.log(pResovePromise)
+// }, 1000)
 
-const pAllPromise = [new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1)
-  }, 1000)
-}), new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(2)
-  }, 5000)
-})]
+// const pAllPromise = [new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(1)
+//   }, 1000)
+// }), new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(2)
+//   }, 5000)
+// })]
 
-JPromise.all(pAllPromise).then((res) => {
-  console.log(res)
-})
+// JPromise.all(pAllPromise).then((res) => {
+//   console.log(res)
+// })
 
 const pRacePromise = [new Promise((resolve, reject) => {
   setTimeout(() => {
